72b5dc01b867fa39254d96b85107ae794d301129
Joseph Janssen <48490003+joej1997@users.noreply.github.com>
2020-06-18 04:16:47 -0700

    Add files via upload

--- R/importAllStationdata.R
+++ R/importAllStationdata.R
@@ -2,23 +2,9 @@
 #' @param fileLoc File path to .dly data
 #' @return nicely organized dataframe of snow data
 #' @export
-library(stringr)
 importAllStationdata<-function(fileLoc){
   SnowDataUpdated <- read.delim(fileLoc, header=FALSE, stringsAsFactors=FALSE)
   SnowDataUpdated<-SnowDataUpdated$V1
-  processStationinfo<-function(curstr){
-    if(stringr::str_length(curstr)==77){
-      id=.Internal(substr(curstr,1L,7L))
-      Name=.Internal(substr(curstr,9L,38L))
-      Lat=.Internal(substr(curstr,40L,45L))
-      Lon=.Internal(substr(curstr,47L,53L))
-      Elev=.Internal(substr(curstr,55L,58L))
-      Sdate=.Internal(substr(curstr,60L,65L))
-      Edate=.Internal(substr(curstr,67L,72L))
-      Nobs=.Internal(substr(curstr,74L,77L))
-      c(id,Name,Lat,Lon,Elev,Sdate,Edate,Nobs)
-    }
-  }
   l<-lapply(SnowDataUpdated, processStationinfo)
   l<-do.call(rbind,l)
   l<-as.data.frame(l)
@@ -28,4 +14,18 @@ importAllStationdata<-function(fileLoc){
   l$Elev<-as.numeric(l$Elev)
   l$Nobs<-as.numeric(l$Nobs)
   l
+}
+
+processStationinfo<-function(curstr){
+  if(stringr::str_length(curstr)==77){
+    id=.Internal(substr(curstr,1L,7L))
+    Name=.Internal(substr(curstr,9L,38L))
+    Lat=.Internal(substr(curstr,40L,45L))
+    Lon=.Internal(substr(curstr,47L,53L))
+    Elev=.Internal(substr(curstr,55L,58L))
+    Sdate=.Internal(substr(curstr,60L,65L))
+    Edate=.Internal(substr(curstr,67L,72L))
+    Nobs=.Internal(substr(curstr,74L,77L))
+    c(id,Name,Lat,Lon,Elev,Sdate,Edate,Nobs)
+  }
 }
