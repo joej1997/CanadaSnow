98a08d575f03282ac0c5aaefdf98214570d827be
Joseph Janssen <48490003+joej1997@users.noreply.github.com>
2020-06-18 03:27:41 -0700

    Add files via upload

---
+++ R/importAllSnowData.R
@@ -0,0 +1,31 @@
+#' Import Canadian snow station information data from .dly file
+#' @param fileLoc File path to .dly data
+#' @return nicely organized dataframe of snow data
+#' @export
+importAllSnowdata<-function(fileLoc){
+  monthlymat<-matrix("",nrow = 31,ncol = 7)
+  monthlymat[,4]=as.character(1:31)
+  l<-lapply(SnowDataUpdated, processStationdata)
+  l<-do.call(rbind,l)
+  l<-as.data.frame(l)
+  colnames(l)<-c("ID","Year","Month","Day","SnowDepth","QC","CA")
+  l$Year<-as.numeric(l$Year)
+  l$Month<-as.numeric(l$Month)
+  l$Day<-as.numeric(l$Day)
+  l$SnowDepth<-as.numeric(l$SnowDepth)
+}
+
+processStationdata<-function(curstr){
+  if(str_length(curstr)!=77){
+    monthlymat[,1]=.Internal(substr(curstr,1L,7L))
+    monthlymat[,2]=.Internal(substr(curstr,9L,12L))
+    monthlymat[,3]=.Internal(substr(curstr,13L,14L))
+    for(j in 1:31){
+      cur<-as.integer((j-1)*10)
+      monthlymat[j,5]<-.Internal(substr(curstr,cur+16L,cur+18L))
+      monthlymat[j,6]<-.Internal(substr(curstr,cur+20L,cur+22L))
+      monthlymat[j,7]<-.Internal(substr(curstr,cur+24L,cur+24L))
+    }
+    monthlymat
+  }
+}
---
+++ R/importAllStationdata.R
@@ -0,0 +1,31 @@
+#' Import Canadian snow station information data from .dly file
+#' @param fileLoc File path to .dly data
+#' @return nicely organized dataframe of snow data
+#' @export
+importAllStationdata<-function(fileLoc){
+  SnowDataUpdated <- read.delim(fileLoc, header=FALSE, stringsAsFactors=FALSE)
+  SnowDataUpdated<-SnowDataUpdated$V1
+  l<-lapply(SnowDataUpdated, processStationinfo)
+  l<-do.call(rbind,l)
+  l<-as.data.frame(l)
+  colnames(l)<-c("ID","Name","Lat","Lon","Elev","Sdate","Edate","Nobs")
+  l$Lat<-as.numeric(l$Lat)
+  l$Lon<-as.numeric(l$Lon)
+  l$Elev<-as.numeric(l$Elev)
+  l$Nobs<-as.numeric(l$Nobs)
+  l
+}
+
+processStationinfo<-function(curstr){
+  if(str_length(curstr)==77){
+    id=.Internal(substr(curstr,1L,7L))
+    Name=.Internal(substr(curstr,9L,38L))
+    Lat=.Internal(substr(curstr,40L,45L))
+    Lon=.Internal(substr(curstr,47L,53L))
+    Elev=.Internal(substr(curstr,55L,58L))
+    Sdate=.Internal(substr(curstr,60L,65L))
+    Edate=.Internal(substr(curstr,67L,72L))
+    Nobs=.Internal(substr(curstr,74L,77L))
+    c(id,Name,Lat,Lon,Elev,Sdate,Edate,Nobs)
+  }
+}
